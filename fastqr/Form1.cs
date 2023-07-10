a082042649c2fcfe5c0228e2489848227832d70f
横茶横葉 <115348123+Yokokiri-Cha@users.noreply.github.com>
2023-07-10 23:35:16 +0900

    Add files via upload

---
+++ fastqr/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace fastqr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo gobr = new ProcessStartInfo();
+            gobr.FileName = "https://api.qrserver.com/v1/create-qr-code/?data=" + textBox1.Text;
+            gobr.UseShellExecute = true;
+            Process.Start(gobr);
+        }
+    }
+}
